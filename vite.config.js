caff071591626f9a3131c7931ba2aebb46be3c27
sivasai-04 <sivasai@validprofile.com>
2026-03-06 22:48:37 +0530

    fix deployment path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this to your GitHub repo name if deploying to GitHub Pages
+  base: '/', 
 })
