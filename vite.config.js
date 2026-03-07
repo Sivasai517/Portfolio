e0f3cabf704fb28e1759026ff611e3cc637235a7
sivasai-04 <sivasai@validprofile.com>
2026-03-07 07:50:15 +0530

    fix asset base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vite configuration
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: "./"
 })
